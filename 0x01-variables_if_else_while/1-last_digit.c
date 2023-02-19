10101a297f64f138b25da85455755ed4229d00d6
Eddie-code-bit <nkansahedward2017@gmail.com>
2023-02-18 20:28:48 -0800

    Remove last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib>
+
+/**
+ * main - Entey
+ * Return - Always 0
+ */
+int main(void)
+{
+	int n;
+	int a;
+
+	srand(time(0));
+	n = rand() RAND_MAX / 2
