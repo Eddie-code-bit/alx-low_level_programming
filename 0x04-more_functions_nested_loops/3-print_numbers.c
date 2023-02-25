d8c83d5449fede624283727dc1b1255b0b30ad5b
Eddie-code-bit <nkansahedward2017@gmail.com>
2023-02-24 21:24:22 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers beween 0 to 9
+ * Return - no return
+ */
+
+void print_numbers(void)
+{
+	int cr;
+
+	for (cr = 48; cr < 58; cr++)
+	{
+		_putchar(cr);
+	}
+	_putchar('\n')
+}
